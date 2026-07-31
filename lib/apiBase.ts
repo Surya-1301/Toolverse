12d6daa9573c409279921976e1cca45d64aec288
Surya Pratap Singh (R3TiR3D) <ss058012@gmail.com>
2026-08-01 05:05:52 +0530

    Update apiBase.ts

--- lib/apiBase.ts
+++ lib/apiBase.ts
@@ -1,7 +1,7 @@
 export function getApiBaseUrl() {
   return (
     process.env.NEXT_PUBLIC_API_BASE_URL ||
-    "https://Toolverse-api.workers.dev"
+    "https://toolversex-api.jethalalmirror.workers.dev"
   ).replace(/\/$/, "");
 }
 
